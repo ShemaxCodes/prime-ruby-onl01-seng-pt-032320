f42d593737ae9ecf759678de898615d64225573f
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 12:01:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,5 @@
-require "prime"
-
 def prime?(num)
-  n = 2
-  while n < num
-    return false 
-    if num % n == 0
-    n += 1
-  end
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end
